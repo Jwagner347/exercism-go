4b021a55a145197138e0a10b073c29883d3b15e8
Jeff Wagner <jeff.wagner@swipesense.com>
2026-08-04 18:59:32 -0500

    append children if node id is not parent id

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -8,6 +8,7 @@ type Record struct {
 
 type Node struct {
 	ID       int
+	ParentID int
 	Children []*Node
 	// feel free to add fields as you see fit
 }
@@ -20,7 +21,14 @@ func Build(records []Record) (*Node, error) {
 	allNodes := make([]*Node, len(records))
 
 	for _, r := range records {
-		allNodes[r.ID] = &Node{ID: r.ID}
+		allNodes[r.ID] = &Node{ID: r.ID, ParentID: r.Parent}
+	}
+
+	for _, n := range allNodes {
+		if n.ParentID != n.ID {
+			allNodes[n.ParentID].Children = append(allNodes[n.ParentID].Children, n)
+		}
+
 	}
 
 	return allNodes[0], nil
